303a00170e9354444db8fe1a80583c5328280329
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-03 13:23:25 +0200

    route for ner

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import io
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from ml import obtain_image
+from ml_images import obtain_image
+from ml_ner import extract_entities
 
 
 app = FastAPI()
@@ -13,3 +14,11 @@ def generate_image(prompt:str):
     image.save(memory_file, "PNG")
     memory_file.seek(0)
     return StreamingResponse(memory_file, media_type="image/png")
+
+
+@app.get("/ner")
+def ner(text:str):
+    entites = extract_entities(text)
+    return entites
+
+
